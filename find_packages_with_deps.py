e8646b12c5c724f46e2f47e19dff96a6e4a9b025
kriswithank <kristopherswann@gmail.com>
2016-12-23 11:35:51 -0600

    Remove use of a global variable

--- find_packages_with_deps.py
+++ find_packages_with_deps.py
@@ -4,14 +4,14 @@ import subprocess
 
 KEEP_PKG = 0
 REMOVE_PKG = 1
-EXIT = False
+EXIT = 2
 
 
 
 def handle_response(term_deps):
     while True:
         response = input('Enter a command (h for help): ')
-        
+
         if response == 'h':
             print("s -> apt show package\np -> pass on package (do nothing)\nh -> help\na -> sudo apt-make auto package\nc -> confirm manual\nq -> quit\nr -> apt rdepends --installed")
         elif response == 's':
@@ -26,29 +26,30 @@ def handle_response(term_deps):
         elif response == 'c':
             return REMOVE_PKG
         elif response == 'q':
-            global EXIT
-            EXIT = True
-            return KEEP_PKG
+            return EXIT
         else:
             print("Invalid response")
-    
-    
 
+
+quit = False
 with open('packages.txt') as source_file, open('packages.result.txt', 'w+') as output_file:
     for line in source_file:
-        
-        if EXIT:
+
+        if quit:
             output_file.write(line)
             continue
-    
+
         term_deps = subprocess.run(['apt', 'rdepends', '--installed', line.strip()], stdout=subprocess.PIPE)
         num_lines = term_deps.stdout.decode("utf-8").count('\n')
-        
+
         if num_lines <= 2:
             output_file.write(line)
         else:
             print(term_deps.stdout.decode("utf-8"))
-           
-            if handle_response(term_deps) == KEEP_PKG:
+            result = handle_response(term_deps)
+
+            if result == KEEP_PKG:
                 output_file.write(line)
-                
+            elif result == EXIT:
+                output_file.write(line)
+                quit = True
