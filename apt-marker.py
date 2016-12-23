bdc74d391657ea9ebc4114235986c0ff898b99e4
kriswithank <kristopherswann@gmail.com>
2016-12-23 12:47:37 -0600

    Add recursive rdepends option

--- apt-marker.py
+++ apt-marker.py
@@ -13,7 +13,8 @@ MENU = """  s -> apt show package
   a -> sudo apt-make auto package
   c -> confirm manual
   q -> quit
-  r -> apt rdepends --installed"""
+  r -> apt rdepends --installed
+  rr -> apt rdepends --installed --recurse"""
 
 KEEP_PKG = 0
 REMOVE_PKG = 1
@@ -21,20 +22,22 @@ EXIT = 2
 
 
 
-def handle_response(term_deps):
+def handle_response(term_rdeps):
     while True:
         response = input('Enter a command (h for help): ')
 
         if response == 'h':
             print(MENU)
         elif response == 's':
-            term_show = subprocess.run(['apt-cache', 'show', term_deps.args[3]])
+            term_show = subprocess.run(['apt-cache', 'show', term_rdeps.args[3]])
         elif response == 'r':
-            print(term_deps.stdout.decode('utf-8'))
+            print(term_rdeps.stdout.decode('utf-8'))
+        elif response == 'rr':
+            term_recurse_rdeps = subprocess.run(term_rdeps.args + ['--recurse',])
         elif response == 'p':
             return KEEP_PKG
         elif response == 'a':
-            term_make_auto = subprocess.run(['sudo', 'apt-mark', 'auto', term_deps.args[3]])
+            term_make_auto = subprocess.run(['sudo', 'apt-mark', 'auto', term_rdeps.args[3]])
             return REMOVE_PKG
         elif response == 'c':
             return REMOVE_PKG
@@ -52,14 +55,14 @@ with open(SOURCE_FILE_NAME) as source_file, open(OUTPUT_FILE_NAME, 'w+') as outp
             output_file.write(line)
             continue
 
-        term_deps = subprocess.run(['apt', 'rdepends', '--installed', line.strip()], stdout=subprocess.PIPE)
-        num_lines = term_deps.stdout.decode("utf-8").count('\n')
+        term_rdeps = subprocess.run(['apt', 'rdepends', '--installed', line.strip()], stdout=subprocess.PIPE)
+        num_lines = term_rdeps.stdout.decode("utf-8").count('\n')
 
         if num_lines <= 2:
             output_file.write(line)
         else:
-            print(term_deps.stdout.decode("utf-8"))
-            result = handle_response(term_deps)
+            print(term_rdeps.stdout.decode("utf-8"))
+            result = handle_response(term_rdeps)
 
             if result == KEEP_PKG:
                 output_file.write(line)
