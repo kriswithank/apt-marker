77cb0af41cce8c48b5e7e846d356ae3f5fea3203
kriswithank <kristopherswann@gmail.com>
2016-12-23 12:51:55 -0600

    Add basic colors

--- apt-marker.py
+++ apt-marker.py
@@ -22,9 +22,21 @@ EXIT = 2
 
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+
 def handle_response(term_rdeps):
     while True:
-        response = input('Enter a command (h for help): ')
+        response = input(bcolors.BOLD + 'Enter a command (h for help): ' + bcolors.ENDC)
 
         if response == 'h':
             print(MENU)
